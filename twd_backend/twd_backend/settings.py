c588af75c678caf495c56e356aaaea5506915690
Marcel Hric <marcelkohric@gmail.com>
2020-09-05 10:10:05 +0200

    Allowed hosts

--- twd_backend/twd_backend/settings.py
+++ twd_backend/twd_backend/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'ye)b6nr4c1v+&&k_89=&sgv_b9j^$11qapik7-=o)4zu6l07rq'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '192.168.0.135']
 
 
 # Application definition
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'api',
-    'rest_framework'
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
